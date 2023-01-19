cc8877dbad7f2f46589baabb23e7f102e4ec0d93
Pasindu Priyashan <47271150+bataishere@users.noreply.github.com>
2023-01-19 22:31:51 +0530

    login success

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="row mb-1">
+                                    <div class="row">
                                         <div class="col-md-12">
                                             <div class="form-check ml-0">
                                                 <input class="form-check-input" type="checkbox" name="remember"
@@ -58,7 +58,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="row mt-3">
+                                    <div class="row mt-2">
                                         <div class="col-md-12">
                                             <button type="submit" class="btn btn-primary w-100">
                                                 {{ __('Login') }}
